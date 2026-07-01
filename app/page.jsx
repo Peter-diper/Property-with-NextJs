dd28c568638d0a488281a11f099fabd5396beec3
Peter <161234440+Peter-diper@users.noreply.github.com>
2026-07-01 08:04:32 +0330

    Adding |  home page footer and box info

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
-
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
   return (
     <>
-      <h1 className="text-center font-bold text-white border order-gray-600 bg-green-400/80 rounded-2xl max-w-140 m-auto  mt-2">
-        Welcome
-      </h1>
-      <Link href="/properties">Show Properties</Link>
+      <Hero />
+      <InfoBoxes />
+      <Footer />
     </>
   );
 };
