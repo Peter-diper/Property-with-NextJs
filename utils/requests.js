2fa0da4c2bb70f96d330ca4c8729463de225cf64
Peter <161234440+Peter-diper@users.noreply.github.com>
2026-07-04 22:58:13 +0330

    adding comment

--- utils/requests.js
+++ utils/requests.js
@@ -4,12 +4,13 @@ async function fetchProperties() {
   try {
     // handle the case where the domain is not available yet
     if (!apiDomain) return [];
-
+    // fetching data whent its avilable
     const res = await fetch(`${apiDomain}/properties`);
 
     if (!res.ok) {
       throw new Error("some thing went wrong");
     }
+    
 
     return res.json();
   } catch (error) {
