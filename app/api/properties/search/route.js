f66ee71f415a3b3df3aea2eeadb915841250465e
peter-diper <rasoolfada2@gmail.com>
2026-07-29 06:44:03 +0330

    feat: implement search API for properties with location and type filtering

---
+++ app/api/properties/search/route.js
@@ -0,0 +1,44 @@
+import connectDB from "@/config/db";
+import Property from "@/models/Property";
+import { NextResponse } from "next/server";
+
+// GET /api/properties/search
+
+export const GET = async (requst) => {
+  try {
+    await connectDB();
+
+    const { searchParams } = new URL(requst.url);
+    const location = searchParams.get("location");
+    const propertyType = searchParams.get("propertyType");
+
+    const locationPattern = new RegExp(location, "i");
+
+    // match location pattern agaist database fields
+    const typePattern = new RegExp(propertyType, "i");
+
+    let query = {
+      $or: [
+        { name: locationPattern },
+        { description: locationPattern },
+        { "location.street": locationPattern },
+        { "location.state": locationPattern },
+        { "location.city": locationPattern },
+        { "location.zipcode": locationPattern },
+        { type: typePattern },
+      ],
+    };
+
+    // only check for property if its not all
+    if (propertyType && propertyType !== "All") {
+      query.type = typePattern;
+    }
+
+    const properties = await Property.find(query);
+
+    return NextResponse.json(properties, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse("something went wrong", { status: 500 });
+  }
+};
