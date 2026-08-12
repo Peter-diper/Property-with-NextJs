250a78c18b14f2a9c83b83e2839a85bee4209094
peter-diper <rasoolfada2@gmail.com>
2026-08-12 23:56:35 +0330

    feat: add dynamic export to HomeProperties component for improved rendering

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import PropertyCard from "./PropertyCard";
 import { fetchProperties } from "@/utils/requests";
 
+export const dynamic = "force-dynamic";
+
 const HomeProperties = async () => {
   const data = await fetchProperties();
 
